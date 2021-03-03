6fabb69e5cb73842f6443d3a789fddb078f59601
Henry Hutchings <henryhutchings@pc-132-243.customer.ask4.lan>
2021-03-03 14:46:49 +0000

    swag

--- matplot.py
+++ matplot.py
@@ -6,3 +6,19 @@ Created on Wed Mar  3 14:42:07 2021
 @author: henryhutchings
 """
 
+import matplotlib.pyplot as plt
+import math
+
+# Create x as a list of 300 numbers equally spaced from 0 to 20
+numpoints = 300
+xmin = 0
+xmax = 20
+delta_x = (xmax - xmin) / (numpoints - 1)
+x = [xmin + i*delta_x for i in range(numpoints)]
+
+# Create y as a list of corresponding y values for y = sin(x)
+y = [math.sin(xi) for xi in x]
+
+# Now plot them:
+plt.plot(x, y)
+plt.show()
